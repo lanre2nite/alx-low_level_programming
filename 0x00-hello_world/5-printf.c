f9d31f9e57a15ae807421c3c9167f9d876c37008
lanre2nite <olabagy@hotmail.com>
2022-12-08 12:55:39 -0800

    My 5-printf.c file commit

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,13 +1,11 @@
 #include <stdio.h>
-
-
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
-int main(void)
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+int main(void) /* int main function*/
 {
-	printf("\"with proper grammar, but the outcome is a piece of art,");
-	return(0);
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
 }
