094601402952e7c19711c23224e641343fda87f6
lanre2nite <olabagy@hotmail.com>
2022-12-08 13:11:57 -0800

    My size.c file commit

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,15 +6,10 @@
 */
 int main(void) /* main function*/
 {
-int i;
-long int ii;
-long long int iii;
-char c;
-float f;
-printf("Size of a char: %d bytes(s)\n", sizeof(c));
-printf("Size of an int: %d byte(s)\n", sizeof(i));
-printf("Size of a long int: %d byte(s)\n", sizeof(ii));
-printf("Size of a long long int: %d bytes(s)\n", sizeof(iii));
-printf("Size of a float: %d byte(s)\n", sizeof(f));
+printf("Size of a char: %d bytes(s)\n", sizeof(char));
+printf("Size of an int: %d byte(s)\n", sizeof(int));
+printf("Size of a long int: %d byte(s)\n", sizeof(long int));
+printf("Size of a long long int: %d bytes(s)\n", sizeof(long long int));
+printf("Size of a float: %d byte(s)\n", sizeof(float));
 return (0);
 }
