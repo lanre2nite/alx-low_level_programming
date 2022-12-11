8b6a5c4c0a55c4816ff842de5dd1a14621e97f20
lanre2nite <olabagy@hotmail.com>
2022-12-11 12:13:55 -0800

    My print_combx files commit

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,29 +1,27 @@
 #include <stdio.h>
 /**
-*main - A c program that prints all two of two digit
-*Returnn: Always 0
+*main- A c program that all possible combinations of two digits
+Return: Always 0
 */
-int main(void) /*Main function*/
+int main(void)
 {
 	int digit1, digit2;
 
-	for (digit1 = 0; digit1 <= 98; digit1++)
+	for (digit1 = 0; digit1 < 9; digit1++)
 	{
-		for (digit2 = digit1 + 1; digit2 <= 99; digit2++)
+		for (digit2 = digit1 + 1; digit2 < 10; digit2++)
 		{
-			putchar((digit1/ 10) + '0');
+			
 			putchar((digit1 % 10) + '0');
-			putchar(32);
-			putchar((digit2 / 10) + '0');
 			putchar((digit2 % 10) + '0');
 
-			if (digit1 / 10 != 9 || digit1 % 10 != 8)
-			{
-				putchar(44);
-				putchar(32);
-			}
+			if (digit1 == 8 && digit2 == 9)
+				continue;
+
+			putchar(',');
+			putchar(' ');
 		}
 	}
-	putchar(10);
+	putchar('\n');
 	return (0);
 }
---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/**
+*main - A c program that prints all two of two digit
+*Returnn: Always 0
+*/
+int main(void) /*Main function*/
+{
+	int digit1, digit2;
+
+	for (digit1 = 0; digit1 <= 98; digit1++)
+	{
+		for (digit2 = digit1 + 1; digit2 <= 99; digit2++)
+		{
+			putchar((digit1/ 10) + '0');
+			putchar((digit1 % 10) + '0');
+			putchar(32);
+			putchar((digit2 / 10) + '0');
+			putchar((digit2 % 10) + '0');
+
+			if (digit1 / 10 != 9 || digit1 % 10 != 8)
+			{
+				putchar(44);
+				putchar(32);
+			}
+		}
+	}
+	putchar(10);
+	return (0);
+}
