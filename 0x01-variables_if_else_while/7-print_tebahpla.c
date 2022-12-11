33b80895203afc2e545144d46bb222e0e657855a
lanre2nite <olabagy@hotmail.com>
2022-12-11 11:30:37 -0800

    My print Hexadecimal file commit

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <ctype.h>
+/**
+*main- A c program that prints all letters in reverse order
+*Return:Always 0
+*/
+int main(void) /*main function*/
+{
+	int letter; /*variable decleration*/
+	
+	for (letter = 'z'; letter >= 'a'; letter--)
+	{
+		putchar(letter);
+	}
+	putchar('\n');
+		return (0);
+}
