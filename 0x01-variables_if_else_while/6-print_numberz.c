b7484a95f2eee2666188f6a303152c1a72591273
lanre2nite <olabagy@hotmail.com>
2022-12-11 11:11:57 -0800

    My print_numberz.c file commit

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <ctype.h>
+/**
+*main-A c program that prints digits of base 10 numbers
+*Return: Always 0
+*/
+int main(void)
+{
+	int digit;
+	for (digit = '0'; digit <= '9'; digit++)
+	{
+		putchar(digit);
+	}
+	putchar('\n');
+	return (0);
+}
