a57b17dfe40f38a376882b32193a82a89fea7511
lanre2nite <olabagy@hotmail.com>
2022-12-12 03:32:23 -0800

    My last_digit file commit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -25,7 +25,7 @@ int main(void)
 		}
 		else if (last < 6)
 		{
-			printf("Last digit of %d is %d and is lass than 6 and not 0\n", n, last);
+			printf("Last digit of %d is %d and is less than 6 and not 0\n", n, last);
 		}
 	}
 	return (0);
