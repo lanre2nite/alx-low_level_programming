e1ae3df779f093efaf744448056b0e03051cabee
lanre2nite <olabagy@hotmail.com>
2022-12-13 11:19:19 -0800

    My commit

--- 0x02-functions_nested_loops/main.c
+++ 0x02-functions_nested_loops/main.c
@@ -1,11 +1,11 @@
 #include "main.h"
 /**
-*main -  check the code
+* main -  check the code
 *
 *Return: Always 0
 */
 int main(void)
 {
-	print_alphabet();
-	return(0);
+print_alphabet_x10();
+return(0);
 }
