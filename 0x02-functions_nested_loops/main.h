3f5009259d2a9235a0ce8c01cc0b814495683acc
lanre2nite <olabagy@hotmail.com>
2022-12-13 05:54:46 -0800

    My main.h file

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,13 +1,14 @@
+#ifndef HOLBERTON_H_INCLUDED
+#define HOLBERTON_H_INCLUDED
 /**
 *File:main.h
 *Author:Pericles Adjovi
 *Desc:header file containing functions declaration
 */
-
 void print_alphabet(void);
 void jack_bauer(void);
 void print_alphabet_x10(void);
-void times_table();
+void times_table(void);
 void print_to_98(int);
 void print_times_table(int);
 int _putchar(char);
@@ -15,5 +16,8 @@ int _islower(int c);
 int _isalpha(int c);
 int print_sign(int n);
 int _abs(int);
-int add(int n, int m);
+int add(int, int);
 int print_last_digit(int n);
+void num(int, int);
+
+#endif
