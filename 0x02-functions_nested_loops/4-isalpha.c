9242c47003308d53f1f92c3b2b0a4fff9c88529a
lanre2nite <olabagy@hotmail.com>
2022-12-13 11:33:15 -0800

    My file

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+*_isalpha - the main code control
+*@c: character to be verified
+* Return: Always 0
+*/
+int _isalpha(int c)
+{
+	if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122))
+		return (1);
+	return(0);
+}
+
