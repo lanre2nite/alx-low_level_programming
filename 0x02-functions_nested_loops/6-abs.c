5036bc692413d52b8b8adc473c3599dc454e8490
lanre2nite <olabagy@hotmail.com>
2022-12-13 12:05:46 -0800

    My files

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+* _abs - to compute the absolute value of an integer
+*@i: targeted number
+*Return - Always 0
+*/
+int _abs(int i)
+{
+if (i > 0)
+{
+return (i);
+}
+else if (i < 0)
+{
+return (-i);
+}
+else
+{
+return (0);
+}
+}
---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+*print_last_digit - print the last digit of a number
+*@n: tergeted number
+*Return: Always 0
+*/
+int print_last_digit(int n)
+{
+	int lastdigit = n % 10;
+
+	if (lastdigit < 0)
+		lastdigit *= -1;
+	putchar (lastdigit + '0');
+	return (lastdigit);
+}
