3328f7f5335fb07dfd8a7c2c71ff3d7fd099d897
lanre2nite <olabagy@hotmail.com>
2022-12-17 06:39:18 -0800

    My 2-mul.c file

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include"main.h"
+/**
+*mul- function to multiple int a and int b
+*@a: int to be multiplied to b
+*@b: int to be multiplied to a
+*Return : return the result for the function 'mul'
+*/
+intmul(int a, int b)
+{
+int c;
+
+c = a * b;
+return (c);
+}
