8a78e04dcd9e512bd494cd9177d329b79a0e633b
lanre2nite <olabagy@hotmail.com>
2022-12-19 01:56:25 -0800

    reset to 98 file

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -17,5 +17,5 @@ void puts2(char *str);
 void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
-int _atoi(chr *s);
+int _atoi(char *s);
 #endif /*MAIN_H*/
