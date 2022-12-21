0dfe8d96f65bd82a8d614ab7f06cc188eff6a0b4
lanre2nite <olabagy@hotmail.com>
2022-12-21 04:46:15 -0800

    My file

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -8,23 +8,15 @@ char *cap_string(char *str)
 {
 	int index = 0;
 
-	while(str[++index])
+	while (str[++index])
 	{
 		while (!(str[index] >= 'a' && str[index] <= 'z'))
 			index++;
-		if (str[index - 1] == ' ' ||
-				str[index - 1] == '\t' ||
-				str[index - 1] == '\n' ||
-				str[index - 1] == ',' ||
-				str[index - 1] == ';' ||
-				str[index - 1] == '.' ||
-				str[index - 1] == '!' ||
-				str[index - 1] == '?' ||
-				str[index - 1] == '"' ||
-				str[index - 1] == '(' ||
-				str[index - 1] == ')' ||
-				str[index - 1] == '{' ||
-				str[index - 1] == '}')
+		if (str[index - 1] == ' ' || str[index - 1] == '\t' ||
+				str[index - 1] == '\n' || str[index - 1] == ',' ||
+				str[index - 1] == ';' || str[index - 1] == '.' || str[index - 1] == '!' ||
+				str[index - 1] == '?' || str[index - 1] == '"' || str[index - 1] == '(' ||
+				str[index - 1] == ')' || str[index - 1] == '{' || str[index - 1] == '}')
 				str[index - 1] -= 32;
 				}
 				return (str);
