c40ff566ac2611368e11acd979b111e2246f9399
lanre2nite <olabagy@hotmail.com>
2023-01-03 00:28:30 -0800

    My file

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,17 +1,21 @@
 #include "main.h"
 /**
- * _memset - a function thar fills memory with a constant byte
- *@s: a pointer named s that fills the first n bytes of the memory area
- *@b: a constant byte
+ *_memset - a function that fills
+ * memory with a constant byte
+ *@s: a pointer named is that fills
+ *the first n bytes of the memory area
+ *@c: a constant byte
  *@n: an integer
+ *description _memset: over there
  *Return: return (s)
  */
-char *_memset(char *s, char b, unsigned int n);
+void *_memset(void *s, int c, size_t n);
 {
-	ubnsigned int index;
-	unsigned char *mem = s, value = b;
+	unsigned int index;
+	unsigned char *mem = s,
+	int value = c;
 
-	for (index = 0; index < n; index++);
+	for (index = 0; index < n; index++)
 	mem[index] = value;
 	return (mem);
 }
--- 0x07-pointers_arrays_strings/_putchar.c
+++ 0x07-pointers_arrays_strings/_putchar.c
@@ -5,7 +5,8 @@
  * @c: the character to print
  * Return: on success 1
  * on error, -1 is returned
- */int _putchar(char c)
+ */
+int _putchar(char c)
 {
 	return (write(1, &c, 1));
 }
