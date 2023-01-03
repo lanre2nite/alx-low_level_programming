41a3af7e20ee5d7067fbc91f362abeabd6626a56
lanre2nite <olabagy@hotmail.com>
2023-01-03 00:36:09 -0800

    My file

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -8,9 +8,11 @@
  */
 char *_memcpy(char *dest, char *src, unsigned int n);
 {
-	unsigned int i = 0;
+	unsigned int i;
 
-	for (; i < n; i++)
+	for (i = 0; n > 0; i++, n--)
+	{
 		dest[i] = src[i];
+	}
 	return (dest);
 }
