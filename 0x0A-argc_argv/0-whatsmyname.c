4c4b8e7b9cff4f705361fc87a08c1d3ea14136ac
lanre2nite <olabagy@hotmail.com>
2023-01-09 08:46:52 -0800

    My task

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -6,7 +6,7 @@
  *
  * Return: always 0 on success
  */
-int main(int _attribute_((_unused_)) argc, char *argv[])
+int main(int __attribute__((__unused__)) argc, char *argv[])
 {
 	printf("%s\n", *argv[0]);
 	return (0);
