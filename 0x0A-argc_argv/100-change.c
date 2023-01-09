6845b930f095e3d980dda846308c4995b3d04b9f
lanre2nite <olabagy@hotmail.com>
2023-01-09 09:01:41 -0800

    My task

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -6,36 +6,43 @@
  *make change for an amount of money
  *@agrc: number of arguments
  *@argv: array of arguments
- *Return: return 0 on success, 1 on Error
+ *Return: return 0 on success
  */
 int main(int argc, char *argv[])
 {
-	int num, j, result;
-
-	int coins[] = {25, 10, 5, 2, 1};
+	int cents, coins = 0;
 
 	if (argc != 2)
 	{
-		printf("Error\n");
-		return (1);
+			printf("Error\n");
+				return (1);
 	}
-	num = atoi(argv[1]);
-
-	result = 0;
-
-	if (num < 0)
+	cents = atoi(argv[1]);
+	while (cents > 0)
 	{
-		printf("0\n");
-		return (0);
+			coins++;
+				if ((cents - 25) >= 0)
+						{
+									cents -= 25;
+											continue;
+												}
+					if ((cents - 10) >= 0)
+							{
+										cents -= 10;
+												continue;
+													}
+						if ((cents - 5) >= 0)
+								{
+											cents -= 5;
+													continue;
+														}
+							if ((cents - 2) >= 0)
+									{
+												cents -= 2;
+														continue;
+															}
+								cents--;
 	}
-	for (j = 0; j < 5 && num >= 0; j++)
-	{
-		while (num >= coins[j])
-		{
-			result++;
-			num -= coins[j];
-		}
-	}
-	printf("%d\n", result);
+	printf("%d\n", coins);
 	return (0);
 }
