868321913007595619098bd7387df32a735f2f3f
lanre2nite <olabagy@hotmail.com>
2023-01-09 05:11:59 -0800

    My task

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -3,11 +3,12 @@
 /**
  * main - prints the name of the program
  * @argc: number of arguments
- * @argv: array of arguments
+ * @*argv: array of arguments
+ *
  * Return: always 0 on success
  */
 int main(int argc_attribute_((unused)), char *argv[])
 {
-	printf("%s\n", *argv[]);
+	printf("%s\n", *argv);
 	return (0);
 }
--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -1,6 +1,6 @@
 #ifndef "main.h"
 #define "main.h"
-
+int main(int argc_attribute_((unused)), char *argv[]);
 int _putchar(char c);
 int _atoi(char *s);
 
