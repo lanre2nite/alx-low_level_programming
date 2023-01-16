a37e25473f3117bbee52c0531a12d23e4f6e39ca
lanre2nite <olabagy@hotmail.com>
2023-01-16 13:03:09 -0800

    My task

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -34,3 +34,43 @@ char *_strcopy(char *dest, char *src)
 	dest[index] = '\0';
 	return (dest);
 }
+/**
+ *new_dog - creates a new dog
+ *@name: name of new dog
+ *@age: age of new dog
+ *@owner: owner of new dog
+ *Return: returns NULL in case
+ *of failure
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *doggo;
+
+	if (name == NULL || age < 0 || owner == NULL)
+		return (NULL);
+
+	doggo = malloc(sizeof(dog_t));
+	if (doggo == NULL)
+		return (NULL);
+
+	doggo->name = malloc(sizeof(char) * (_strlen(name) + 1));
+	if (doggo->name == NULL)
+	{
+		free(doggo);
+		return (NULL);
+	}
+
+	doggo->owner = malloc(sizeof(char) * (_strlen(owner) + 1));
+	if (doggo->owner == NULL)
+	{
+		free(doggo->name);
+		free(doggo);
+		return (NULL);
+	}
+
+	doggo->name = _strcopy(doggo->name, name);
+	doggo->age = age;
+	doggo->owner = _strcopy(doggo->owner, owner);
+
+	return (doggo);
+}
