daa82b2413c4711aa7e0252984e97eb0438d0828
lanre2nite <olabagy@hotmail.com>
2023-01-18 20:20:04 -0800

    My task

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,18 @@
+#include "function_pointers.h"
+/**
+ *array_iterator - executes a function on each ...
+ @array: array to be targeted
+ @size: size of an array
+ @action: function to be executed
+ *
+ */
+void array_iterator(int *array, size_t size, coid (*action)(int))
+{
+	if (array == NULL || action == NULL)
+		return;
+	while (size-- > 0)
+	{
+		action(*array);
+		array++;
+	}
+}
