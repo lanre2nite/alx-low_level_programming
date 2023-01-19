9f95b3692f85d08c3fea86227eb26e7f685bf886
lanre2nite <olabagy@hotmail.com>
2023-01-18 20:44:27 -0800

    My task

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,9 +1,9 @@
 #include "function_pointers.h"
 /**
- *array_iterator - executes a function on each ...
- @array: array to be targeted
- @size: size of an array
- @action: function to be executed
+ * array_iterator - executes a function on each
+ * @array: array to be targeted
+ *@size: size of an array
+ *@action: function to be executed
  *
  */
 void array_iterator(int *array, size_t size, void (*action)(int))
