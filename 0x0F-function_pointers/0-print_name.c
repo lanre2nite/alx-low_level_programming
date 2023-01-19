88e08be13f9bfd0c59f6100fef9844ec38b2856b
lanre2nite <olabagy@hotmail.com>
2023-01-18 20:15:10 -0800

    My task

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,12 @@
+#include "function_pointers.h"
+/**
+ * print_name - prints a name
+ * @name: name to be printed
+ * @f: function which prints the name
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (name == NULL || f == NULL)
+		return;
+	f(name);
+}
