3534b3d011bf922200eb378af8f35b2a195969f0
lanre2nite <olabagy@hotmail.com>
2023-01-19 17:14:04 -0800

    My task

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -1,6 +1,4 @@
 #include "variadic_functions.h"
-#include <stdio.h>
-#include <stdarg.h>
 /**
  * print_numbers - prints numbers, followed by a new line
  * @separator: the string to be printed btw numbers
