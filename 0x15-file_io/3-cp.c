292a06202114f8a5c959194aecdc6dfea37f82fb
lanre2nite <olabagy@hotmail.com>
2023-02-06 11:45:09 -0800

    My task

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -13,9 +13,9 @@ void close_file(int fd);
  */
 char *create_buffer(char *file)
 {
-	char *buffer;
+char *buffer;
 
-	buffer = malloc(sizeof(char) * 1024);
+buffer = malloc(sizeof(char) * 1024);
 	if (buffer == NULL)
 	{
 		dprintf(STDERR_FILENO,
