fc862292adec3544f2bfe1a02d6a4d3d90555f3c
lanre2nite <olabagy@hotmail.com>
2023-02-06 11:49:57 -0800

    My task

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -12,18 +12,18 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
+char *buf;
 ssize_t fd;
 ssize_t w;
 ssize_t t;
-char *buffer;
 
-fd = open(filename, 0_RDONLY);
+fd = open(filename, O_RDONLY);
 if (fd == -1)
 return (0);
-buffer = malloc(sizeof(char) * letters);
-t = read(fd, buffer, letters);
-w = write(STDOUT_FILENO, buffer, t);
-free(buffer);
+buf = malloc(sizeof(char) * letters);
+t = read(fd, buf, letters);
+w = write(STDOUT_FILENO, buf, t);
+free(buf);
 close(fd);
 return (w);
 }
