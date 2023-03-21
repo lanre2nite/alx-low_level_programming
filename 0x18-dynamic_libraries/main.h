f0774a8693392e54685f6253f68107a46434b5e5
lanre2nite <olabagy@hotmail.com>
2023-03-21 08:37:32 -0700

    My task

--- 0x18-dynamic_libraries/main.h
+++ 0x18-dynamic_libraries/main.h
@@ -1,5 +1,5 @@
-#ifndef _MAIN_H_
-#define _MAIN_H_
+#ifndef __MAIN__
+#define __MAIN__
 
 int _putchar(char c);
 int _islower(int c);
@@ -21,6 +21,7 @@ char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
+int nrandom();
 
 #endif /* _MAIN_H */
 
